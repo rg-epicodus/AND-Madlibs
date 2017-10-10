a6a8ad7c00eb6fc6a15fd11303c2c5b4cfbc7453
Guest User <Guest@Epicodus-4B.local>
2017-10-10 09:34:55 -0700

    add initial files

---
+++ app/src/test/java/mymadlibs/epicodus/com/mymadlibs/MainActivityTest.java
@@ -0,0 +1,32 @@
+package mymadlibs.epicodus.com.mymadlibs;
+
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertTrue;
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent() {
+        TextView appNameTextView = (TextView) activity.findViewById(R.id.appNameTextView);
+        assertTrue("Welcome to our MadLib madly silly stories where you are in control of the storyline! Choose from a list of possible stories, fill in the words, and unveil your final story.  Let's start the fun!".equals(appNameTextView.getText().toString()));
+    }
+}
+
